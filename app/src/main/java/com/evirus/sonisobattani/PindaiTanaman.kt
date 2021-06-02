0d4666386e961a7da88410d02b80e2ea13be44e4
Egi <a2832581@bangkit.academy>
2021-06-02 15:34:45 +0700

    fourth commit

--- app/src/main/java/com/evirus/sonisobattani/PindaiTanaman.kt
+++ app/src/main/java/com/evirus/sonisobattani/PindaiTanaman.kt
@@ -7,15 +7,18 @@ import android.graphics.Bitmap
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.provider.MediaStore
 import android.util.Base64
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import com.evirus.sonisobattani.databinding.ActivityPindaiTanamanBinding
 import java.io.ByteArrayOutputStream
 
 class PindaiTanaman : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
         private val REQUEST_CODE = 7
         private lateinit var binding:ActivityPindaiTanamanBinding
         override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,8 +26,10 @@ class PindaiTanaman : AppCompatActivity() {
             super.onCreate(savedInstanceState)
             binding=ActivityPindaiTanamanBinding.inflate(layoutInflater)
             setContentView(binding.root)
-
+            binding.btnDown.isEnabled = false
+            binding.btnUp.isEnabled = false
             binding.btnTake.setOnClickListener {
+
                 val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
 
                 if (takePictureIntent.resolveActivity(this.packageManager) != null) {
@@ -32,24 +37,48 @@ class PindaiTanaman : AppCompatActivity() {
                 } else {
                     Toast.makeText(this, "Unable to use camera", Toast.LENGTH_SHORT).show()
                 }
-
+                binding.btnUp.isEnabled = true
             }
         }
 
         override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-            binding=ActivityPindaiTanamanBinding.inflate(layoutInflater)
+            binding= ActivityPindaiTanamanBinding.inflate(layoutInflater)
             setContentView(binding.root)
             if (requestCode== REQUEST_CODE && resultCode== Activity.RESULT_OK){
                 val takenImage= data?.extras?.get("data") as Bitmap
                 binding.imageView.setImageBitmap(takenImage)
-                FirebaseStorageManager().uploadImage(this, getImageUriFromBitmap(this,takenImage))
+                binding.btnDown.isEnabled = false
+                // FirebaseStorageManager().uploadImage(this, getImageUriFromBitmap(this,takenImage))
+                binding.btnUp.setOnClickListener {
+                    binding.progressBar.visibility= View.VISIBLE
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        Toast.makeText(this, "Berhasil Mengunggah", Toast.LENGTH_SHORT).show()
+                        binding.progressBar.visibility= View.INVISIBLE
+                        binding.btnDown.isEnabled = true
+                        binding.btnUp.isEnabled = false
+                        binding.textView2.visibility=View.VISIBLE
+                    }, 1000)
+                }
+                binding.btnDown.setOnClickListener {
+                    binding.progressBar.visibility= View.VISIBLE
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        Toast.makeText(this, "Berhasil Mengunduh Solusi", Toast.LENGTH_SHORT).show()
+                        binding.progressBar.visibility= View.INVISIBLE
+                        binding.btnUp.isEnabled = false
+                        binding.textView2.visibility=View.VISIBLE
+                        val intent= Intent(this, SolusiActivity::class.java)
+                        startActivity(intent)
+                    }, 1000)
+                }
             }else{ super.onActivityResult(requestCode, resultCode, data)}
 
         }
-        fun getImageUriFromBitmap(context: Context, bitmap: Bitmap): Uri{
+        fun getImageUriFromBitmap(context: Context, bitmap: Bitmap): Uri {
             val bytes = ByteArrayOutputStream()
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+            val quality = 100
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, bytes)
             val path = MediaStore.Images.Media.insertImage(context.contentResolver, bitmap, "Title", null)
             return Uri.parse(path.toString())
         }
     }
+}
---
+++ app/src/main/java/com/evirus/sonisobattani/SolusiActivity.kt
@@ -0,0 +1,11 @@
+package com.evirus.sonisobattani
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SolusiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_solusi)
+    }
+}
